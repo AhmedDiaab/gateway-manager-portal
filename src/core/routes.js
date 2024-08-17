9cfca4fc21da4c5df4c192f44d7dfb4de326a3bb
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 18:48:22 +0300

    feat(): create initial setup for project

---
+++ src/core/routes.js
@@ -0,0 +1,17 @@
+import React, { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
+// lazy load pages
+const HomePage = lazy(() => import('../pages/Home'));
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/todos" element={<TodoPage />} /> */}
+        </Routes>
+    );
+};
+
+export default AppRoutes;
