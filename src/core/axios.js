f83ac7d21fd42db4746983c1ba99bd768fbb14c7
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 22:24:49 +0300

    feat(): add fallback for env variable

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL, // TODO: Replace with your API base URL
+  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3030/api/v1", // TODO: Replace with your API base URL
 });
 
 console.log(process.env.REACT_APP_BASE_URL)
